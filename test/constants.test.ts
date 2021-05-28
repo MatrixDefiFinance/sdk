85d7503025cca09014ff55b7efd496487a0a213f
Mohamed Doffer Mohamed Nasir <73598690+mohamed-nasir@users.noreply.github.com>
2021-05-28 21:03:45 +0800

    v0.0.1

---
+++ test/constants.test.ts
@@ -0,0 +1,20 @@
+import { INIT_CODE_HASH } from '../src/constants'
+
+//import { bytecode } from '@uniswap/v2-core/build/UniswapV2Pair.json'
+//import { keccak256 } from '@ethersproject/solidity'
+
+// this _could_ go in constants, except that it would cost every consumer of the sdk the CPU to compute the hash
+// and load the JSON.
+//const COMPUTED_INIT_CODE_HASH = keccak256(['bytes'], [`0x${bytecode}`])
+
+describe('constants', () => {
+  describe('INIT_CODE_HASH', () => {
+    // it('matches computed bytecode hash', () => {
+    //   expect(COMPUTED_INIT_CODE_HASH).toEqual(INIT_CODE_HASH)
+    // })
+    //TODO: Replace with real PancakePair.json
+    it('matches computed bytecode hash', () => {
+      expect(INIT_CODE_HASH).toEqual('0x566faae40390aaa00be48ecd9ebeab74d16e8399eef6ecd0f1cb05c2908fa0fa')
+    })
+  })
+})
